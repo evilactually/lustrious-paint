aaffd4d8fd56501116416ad882075cd39e93da46
Georgy Curnoff <geocurnoff@gmail.com>
2015-12-15 20:38:12 -0700

    rewrite, separating out WndProc into handlers

--- main.cpp
+++ main.cpp
@@ -1,245 +1,261 @@
 
 #include <windows.h>
-#include <windowsx.h>
-#include "resource.h"
-#include <sstream>
+#include <Windowsx.h>
+#include <exception>
 
-enum GRAB_STATE;
+HINSTANCE hInstance;
+HWND      hWnds[2];
 
-class Window {
-	// [events]
-	void SetCursorImage();
-	void SetCursorVisibility();
-	void SetPosition();
-	void SetSize();
-	
-	// minimize
-	// maximize
-	// restore
-	void Maximize();
-	void Minimize();
-	void Restore();
-	void Update();
-	void BeginGrab(GRAB_STATE target);
-	void EndGrab();
+enum {
+	HWNDS_MAIN_WINDOW,
+	HWNDS_OPEN_DIALOG_WINDOW
 };
 
-enum GRAB_STATE {
-	GRB_NOTHING,
-	GRB_WINDOW, 
-	GRB_TOP_EDGE, 
-	GRB_RIGHT_EDGE, 
-	GRB_BOTTOM_EDGE, 
-	GRB_LEFT_EDGE, 
-	GRB_TOP_LEFT_CORNER,
-	GRB_TOP_RIGHT_CORNER,
-	GRB_BOTTOM_RIGHT_CORNER,
-	GRB_BOTTOM_LEFT_CORNER
+struct Win32MsgHandlerResponse {
+	BOOL    pass;   // indicate that next handler should be invoked
+	LRESULT result; // value passed to next handler or to Windows
 };
 
-HWND  m_hwnd;
-HINSTANCE	hinst;
-POINT m_grab_point = {0,0};
-bool  m_is_grabbed = false;
-GRAB_STATE m_grab_state;
-int m_grab_width;
-RECT m_original_rect;
 
-void BeginGrab(GRAB_STATE target) {
-	// calculate and save grab point(in window coordinates)
-	//RECT r;
-	//POINT c;
-	//GetCursorPos(&c);
-	
-	//GetWindowRect(m_hwnd, &r);
-	//m_grab_point = { c.x - r.left, c.y - r.top };
-	//m_grab_width = r.right - r.left;
+template<class T>
+class Just;
 
-	GetCursorPos(&m_grab_point);
-	GetWindowRect(m_hwnd, &m_original_rect);
+template<class T>
+class Nothing;
 
-	// allow mouse to get over the edge and still recieve mouse events
-	SetCapture(m_hwnd);
-
-	m_grab_state = target;
-}
-
-void EndGrab() {
-	m_grab_state = GRB_NOTHING;
-	ReleaseCapture();
-}
-
-void BeginWindowGrab() {
-	// calculate and save grab point(in window coordinates)
-	RECT r;
-	POINT c;
-	GetCursorPos(&c);
-	GetWindowRect(m_hwnd, &r);
-	m_grab_point = { c.x - r.left, c.y - r.top };
-
-	// allow mouse to get over the edge and still recieve mouse events
-	SetCapture(m_hwnd);
-
-	m_is_grabbed = true;
+template<class T>
+class Maybe {
+friend Just<T>;
+protected:
+	T m_value;
+	bool m_is_just;
+	Maybe() : m_is_just(false) {};
+	Maybe(T value, bool is_just) : m_value(value), m_is_just(is_just) {};
+public:
+	Maybe(Just<T>& just) : Maybe(just.m_value, true) {}
+	Maybe(Nothing<T>& nothing) : Maybe() {}
+	bool isJust() {
+		return m_is_just;
+	}
+	bool isNothing() {
+		return !m_is_just;
+	}
 };
 
-void EndWindowGrab() {
-	ReleaseCapture();
-	m_is_grabbed = false;
+template<class T>
+class Just : public Maybe<T> {
+public:
+	Just(T value) : Maybe(value, true) {}
+	Just(Maybe<T>& maybe) {
+		if (maybe.m_is_just) {
+			Just(maybe.m_value);
+		}
+		else {
+			throw std::range_error("Nothing<T> cannot be converted to Just<T>");
+		}
+	}
+	T GetValue() {
+		return m_value;
+	}
+};
+
+template<class T>
+class Nothing : public Maybe<T> {
+public:
+	Nothing() : Maybe() {}
+};
+
+void test() {
+	Maybe<double> v01 = Nothing<double>();
+	Maybe<double> v02 = Just<double>(2.0);
+	if (v01.isJust()) {
+		double value = Just<double>(v01).GetValue();
+	}
+	Just<double> v03 = v02;
+	if (v01.isJust())
+	  Just<double> v04 = v01;
 }
 
-LRESULT CALLBACK WindowProc(
+/* A message handler is a piece of Window behavior */
+
+class Win32MsgHandler {
+protected:
+	const UINT m_message_type;
+	Win32MsgHandler(UINT message_type) : m_message_type(message_type) {};
+public:
+	virtual Maybe<LRESULT> HandleMessage(HWND hWnd, WPARAM wParam, LPARAM lParam) = 0;
+};
+
+class Win32BorderHitTestHandler : public Win32MsgHandler {
+protected:
+	LONG m_border_width;
+public:
+	Win32BorderHitTestHandler(LONG border_width) : Win32MsgHandler(WM_NCHITTEST), m_border_width(border_width) { };
+	Maybe<LRESULT> HandleMessage(HWND hWnd, WPARAM wParam, LPARAM lParam) override {
+		RECT winrect;
+		GetWindowRect(hWnd, &winrect);
+		long x = GET_X_LPARAM(lParam);
+		long y = GET_Y_LPARAM(lParam);
+
+		//bottom left corner
+		if (x >= winrect.left && x < winrect.left + m_border_width &&
+			y < winrect.bottom && y >= winrect.bottom - m_border_width)
+		{
+			return Just<LRESULT>(HTBOTTOMLEFT);
+		}
+		//bottom right corner
+		if (x < winrect.right && x >= winrect.right - m_border_width &&
+			y < winrect.bottom && y >= winrect.bottom - m_border_width)
+		{
+			return Just<LRESULT>(HTBOTTOMRIGHT);
+		}
+		//top left corner
+		if (x >= winrect.left && x < winrect.left + m_border_width &&
+			y >= winrect.top && y < winrect.top + m_border_width)
+		{
+			return Just<LRESULT>(HTTOPLEFT);
+		}
+		//top right corner
+		if (x < winrect.right && x >= winrect.right - m_border_width &&
+			y >= winrect.top && y < winrect.top + m_border_width)
+		{
+			return Just<LRESULT>(HTTOPRIGHT);
+		}
+		//left border
+		if (x >= winrect.left && x < winrect.left + m_border_width)
+		{
+			return Just<LRESULT>(HTLEFT);
+		}
+		//right border
+		if (x < winrect.right && x >= winrect.right - m_border_width)
+		{
+			return Just<LRESULT>(HTRIGHT);
+		}
+		//bottom border
+		if (y < winrect.bottom && y >= winrect.bottom - m_border_width)
+		{
+			return Just<LRESULT>(HTBOTTOM);
+		}
+		//top border
+		if (y >= winrect.top && y < winrect.top + m_border_width)
+		{
+			return Just<LRESULT>(HTTOP);
+		}
+
+		// invoke next handler
+		return Nothing<LRESULT>();
+	};
+};
+
+#include <map>
+#include <deque>
+
+class Win32WndProc;
+
+class Win32WndProcDescriptor final {
+public:
+	friend Win32WndProc;
+	void push_handler_back(Maybe<UINT> message_type, Win32MsgHandler* handler) {
+		if (message_type.isJust()) {
+			m_handlers_map[Just<UINT>(message_type).GetValue()].push_back(handler);
+		}
+		else {
+			m_default_handlers.push_back(handler);
+		}
+	}
+	void push_handler_front(Maybe<UINT> message_type, Win32MsgHandler* handler) {
+		if (message_type.isJust()) {
+			m_handlers_map[Just<UINT>(message_type).GetValue()].push_front(handler);
+		}
+		else {
+			m_default_handlers.push_front(handler);
+		}
+	}
+private:
+	std::map<UINT, std::deque<Win32MsgHandler*>> m_handlers_map;
+	std::deque<Win32MsgHandler*>                 m_default_handlers;
+
+	//std::map<UINT, Win32MsgHandler**> ConstructHandlerStackMap() {
+	//	std::map<UINT, Win32MsgHandler**> handlers_map_final;
+	//	for each (auto pair in m_handlers_map)
+	//	{
+	//		Win32MsgHandler** handles_array = new Win32MsgHandler*[pair.second.size() + 1];
+	//		handlers_map_final[pair.first] = handles_array;              // associate with message
+	//		handles_array[pair.second.size()] = nullptr;                 // null terminate
+	//		copy(pair.second.begin(), pair.second.end(), handles_array); // copy deque into array
+	//	}
+	//	return handlers_map_final;
+	//}
+
+	//Win32MsgHandler** ConstructDefaultHandlerStack() {
+	//	Win32MsgHandler** default_handlers_final = new Win32MsgHandler*[m_default_handlers.size() + 1];
+	//	return default_handlers_final;
+	//}
+};
+
+/* Win32WndProc assembles Win32MsgHandlers into a WndProc that can be used with Win32 Api */
+
+class Win32WndProc final {
+private:
+	std::map<UINT, Win32MsgHandler**> m_handlers_map;     // message handler stacks for some or all message types
+	Win32MsgHandler**                 m_default_handlers; // default message handler stack used for messages not in the handler map
+
+	Win32WndProc(std::map<UINT, Win32MsgHandler**> handlers_map) : m_handlers_map(handlers_map) { }
+
+	~Win32WndProc() 
+	{ 
+		for each (auto pair in m_handlers_map) 
+			delete[] pair.second; 
+	}
+
+	/* Invoke handlers in order until one of them returns final result */
+	LRESULT invoke_handlers(
+		Win32MsgHandler** handlers, 
+		HWND	hWnd,
+		UINT	uMsg,
+		WPARAM	wParam,
+		LPARAM	lParam) 
+	{
+		// iterate null-terminated array
+		for (Win32MsgHandler** i = handlers; *i; ++i) {
+			Win32MsgHandler* handler = *i;
+			Maybe<LRESULT> response = handler->HandleMessage(hWnd, wParam, lParam);
+			if (response.isJust()) {
+				return Just<LRESULT>(response).GetValue();
+			}
+		}
+	}
+public:
+	LRESULT CALLBACK WndProc(
+		HWND	hWnd,
+		UINT	uMsg,
+		WPARAM	wParam,
+		LPARAM	lParam)
+	{
+		auto found = m_handlers_map.find(uMsg);
+		if (found != m_handlers_map.end()) {
+			return invoke_handlers(found->second, hWnd, uMsg, wParam, lParam);
+		}
+		else {
+			return invoke_handlers(m_default_handlers, hWnd, uMsg, wParam, lParam);
+		}
+	}
+
+};
+
+/* A redirect is needed because member function pointers are incompatible with regular function pointers */
+
+LRESULT CALLBACK WndProcRedirect(
 	HWND	hWnd,
 	UINT	uMsg,
 	WPARAM	wParam,
 	LPARAM	lParam)
 {
-	HDC         hdc;
-	PAINTSTRUCT ps;
-	RECT        rect;
-	static int count = 0;
-
-	// sort through and find what code to run for the message given
-	switch (uMsg)
-	{
-	case WM_NCHITTEST: {
-		return HTCAPTION;
-		break;
-	}
-	case WM_GETICON: {
-		/*RECT wr{ 0,0,640,480 };
-		InvalidateRect(hWnd, &wr, true);
-		UpdateWindow(hWnd);*/
-		
-		if (count % 2) {
-			return  (LRESULT)LoadIcon(hinst, MAKEINTRESOURCE(IDI_LUSTRIOUS_PAINT));
-		}
-		else {
-			return  (LRESULT)LoadIcon(hinst, MAKEINTRESOURCE(IDI_WARNING));
-		}
-		break;
-	}
-	case WM_LBUTTONDOWN: {
-
-		//SetClassLong(hWnd, GCL_HICON, (LONG)LoadIcon(NULL, IDI_APPLICATION));
-		//SetClassLong(hWnd, GCL_HCURSOR, (LONG)LoadCursor(NULL, IDC_HAND));
-		count++;
-		//ShowWindow(hWnd, SW_MINIMIZE);
-
-		//BeginGrab(GRB_WINDOW);
-		//BeginGrab(GRB_RIGHT_EDGE);
-		//BeginGrab(GRB_LEFT_EDGE);
-		//BeginGrab(GRB_BOTTOM_EDGE);
-		//BeginGrab(GRB_TOP_EDGE);
-		//BeginGrab(GRB_BOTTOM_RIGHT_CORNER);
-		//BeginGrab(GRB_TOP_RIGHT_CORNER);
-		//BeginGrab(GRB_BOTTOM_LEFT_CORNER);
-		//BeginGrab(GRB_TOP_LEFT_CORNER);
-		//SendMessageA(hWnd, 0, NULL, NULL);
-		
-		break;
-	}
-	case WM_LBUTTONUP: {
-		EndGrab();
-		break;
-	}
-	case WM_MOUSEMOVE: {
-		if (m_grab_state != GRB_NOTHING) {
-			POINT final_point;
-			GetCursorPos(&final_point);
-			POINT drag_delta = { final_point.x - m_grab_point.x, final_point.y - m_grab_point.y };
-			int original_width = m_original_rect.right - m_original_rect.left;
-			int original_height = m_original_rect.bottom - m_original_rect.top;
-
-			switch (m_grab_state)
-			{
-			case GRB_WINDOW: {
-				MoveWindow(hWnd, m_original_rect.left + drag_delta.x, m_original_rect.top + drag_delta.y, original_width, original_height, false);
-				break;
-			}
-			case GRB_RIGHT_EDGE:
-				MoveWindow(hWnd, m_original_rect.left, m_original_rect.top, original_width + drag_delta.x, original_height, false);
-				break;
-			case GRB_LEFT_EDGE: 
-				MoveWindow(hWnd, m_original_rect.left + drag_delta.x, m_original_rect.top, original_width - drag_delta.x, original_height, false);
-				break;
-			case GRB_BOTTOM_EDGE:
-				MoveWindow(hWnd, m_original_rect.left, m_original_rect.top, original_width, original_height + drag_delta.y, false);
-				break;
-			case GRB_TOP_EDGE:
-				MoveWindow(hWnd, m_original_rect.left, m_original_rect.top + drag_delta.y, original_width, original_height - drag_delta.y, false);
-				break;
-			case GRB_BOTTOM_RIGHT_CORNER:
-				MoveWindow(hWnd, m_original_rect.left, m_original_rect.top, original_width + drag_delta.x, original_height + drag_delta.y, false);
-				break;
-			case GRB_TOP_RIGHT_CORNER:
-				MoveWindow(hWnd, m_original_rect.left, m_original_rect.top + drag_delta.y, original_width + drag_delta.x, original_height - drag_delta.y, false);
-				break;
-			case GRB_BOTTOM_LEFT_CORNER:
-				MoveWindow(hWnd, m_original_rect.left + drag_delta.x, m_original_rect.top, original_width - drag_delta.x, original_height + drag_delta.y, false);
-				break;
-			case GRB_TOP_LEFT_CORNER:
-				MoveWindow(hWnd, m_original_rect.left + drag_delta.x, m_original_rect.top + drag_delta.y, original_width - drag_delta.x, original_height - drag_delta.y, false);
-				break;
-			default:
-				break;
-			}
-		
-		}
-
-		//if (m_grab_state == GRB_WINDOW) {
-		//	POINT abs_xy;
-		//	GetCursorPos(&abs_xy);
-		//	RECT rct;
-		//	GetWindowRect(hWnd, &rct);
-		//	MoveWindow(hWnd, abs_xy.x - m_grab_point.x, abs_xy.y- m_grab_point.y, rct.right-rct.left, rct.bottom-rct.top, false);
-		//	//MoveWindow(hWnd, 100, abs_xy.y - cl_y, 640, 480, false);
-		//	/*RECT wr = { 0, 0, 640, 480 };
-		//	InvalidateRect(hWnd, &wr, true);
-		//	UpdateWindow(hWnd);
-		//	SetCapture(hWnd);*/
-		//}
-		//if (m_grab_state == GRB_RIGHT_EDGE) {
-		//	POINT abs_xy;
-		//	GetCursorPos(&abs_xy);
-		//	RECT rct;
-		//	GetWindowRect(hWnd, &rct);
-		//	MoveWindow(hWnd, rct.left, rct.top, m_grab_width + (abs_xy.x - m_grab_point.x), rct.bottom - rct.top, false);
-		//}
-		break;
-	}
-	case WM_DESTROY:
-	{
-		// close the application entirely
-		PostQuitMessage(0);
-		return 0;
-	} break;
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		GetClientRect(hWnd, &rect);
-		std::stringstream ss;
-		ss << count;
-		//FillRect(hdc, &rect, (HBRUSH)GetStockObject(WHITE_BRUSH));
-		DrawText(hdc, ss.str().c_str(), -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-		count++;
-		EndPaint(hWnd, &ps);
-		break;
-	}
-
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	Win32WndProc* instance = (Win32WndProc*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+	return instance->WndProc(hWnd, uMsg, wParam, lParam);
 }
 
-// #define WS_STYLE 0x860f0000 
-//                    820a0000
-#define  WS_STYLE WS_POPUP |/* WS_CLIPCHILDREN | WS_CLIPSIBLINGS |*/ WS_SYSMENU | WS_THICKFRAME /*| WS_GROUP  |*/ | WS_TABSTOP
-//|  WS_OVERLAPPEDWINDOW
-//#define WS_STYLE WS_OVERLAPPEDWINDOW
-// #define WS_STYLE /*WS_OVERLAPPEDWINDOW WS_POPUP*/  (WS_OVERLAPPED | WS_CAPTION | /*WS_SYSMENU |*/ WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX) | WS_EX_LAYERED 
-//#define WS_STYLE WS_POPUP | WS_CLIPCHILDREN | WS_GROUP | WS_SYSMENU | WS_BORDER | WS_CLIPSIBLINGS
-//#define WS_STYLE WS_POPUP | WS_SIZEBOX
-//#define WS_STYLE WS_POPUP | WS_OVERLAPPED
-//#define WS_STYLE WS_POPUP | WS_BORDER | WS_SYSMENU
-//#define WS_STYLE WS_POPUP | WS_SIZEBOX
+typedef LRESULT(CALLBACK WndProcCallback)(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(
 	HINSTANCE	hInstance,
@@ -247,71 +263,22 @@ int WINAPI WinMain(
 	LPSTR		lpCmdLine,
 	int			nCmdShow)
 {
-	hinst = hInstance;
-
-	// the handle for the window, filled by a function
-	//HWND hWnd;
-	// this struct holds information for the window class
-	WNDCLASSEX wc;
-
-	// Get window size
-	RECT wr = { 0, 0, 640, 480 };
-	AdjustWindowRect(&wr, WS_STYLE, FALSE);
-
-	// clear out the window class for use
-	ZeroMemory(&wc, sizeof(WNDCLASSEX));
-
-	// fill in the struct with the needed information
-	wc.cbSize = sizeof(WNDCLASSEX);
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInstance; 
-	wc.hIcon = NULL; // LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LUSTRIOUS_PAINT));
-	wc.hIconSm = NULL; // LoadIcon(hInstance, IDI_APPLICATION);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)COLOR_BACKGROUND;
-	//wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-	//wc.hbrBackground = NULL;
-	wc.lpszClassName = "WindowClass";
-
-	// register the window class
-	RegisterClassEx(&wc);
-
-	// create the window and use the result as the handle
-	m_hwnd = CreateWindowEx(
-		NULL,
-		"WindowClass",        // class name
-		"Lustrious Paint",    // title
-		WS_STYLE,             // style
-		100,                  // x-position
-		100,                  // y-position
-		wr.right-wr.left,     // width
-		wr.bottom-wr.top,     // height
-		NULL,                 // no parent
-		NULL,                 // no menus
-		hInstance,
-		NULL);                // used with multiple windows, NULL
-
-	ShowWindow(m_hwnd, nCmdShow);
 	
-	SetWindowLong(m_hwnd, GWL_EXSTYLE, GetWindowLong(m_hwnd, GWL_EXSTYLE) | WS_EX_LAYERED);
-	SetLayeredWindowAttributes(m_hwnd, RGB(0,0,0), 0, LWA_COLORKEY);
+	Win32BorderHitTestHandler w(5);
+	//Win32MsgHandlerMap mm01;
+	//Win32MsgHandlerMap mm02;
 
-	// enter the main loop:
+	//std::map<UINT, std::vector<Win32MsgHandler*>> m_handlers;
+	//m_handlers[1].push_back(nullptr);
+	//WndProcCallback* f01 = &mm01.WindowProc;
+	//WndProcCallback* f02 = &mm02.WindowProc;
+	//if (f01 == f02) {
+	//	MessageBeep(MB_ICONASTERISK);
+//	}
 
-	// this struct holds Windows event messages
-	MSG msg;
-
-	// wait for the next message in the queue, store the result in 'msg'
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		// translate keystroke messages into the right format
-		TranslateMessage(&msg);
-
-		// send the message to the WindowProc function
-		DispatchMessage(&msg);
-	}
-
-	// return this part of the WM_QUIT message to Windows
-	return msg.wParam;
+	//Win32BorderHitTestHandler::instance.HandleMessage(0,0,0,0,0);
+	// init everything
+	// register classes
+	// create all windows
+	return 0;
 }
