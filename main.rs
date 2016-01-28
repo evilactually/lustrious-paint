171c61e2f98180348fd49073f22803e3075a9f63
Georgy Curnoff <geocurnoff@gmail.com>
2016-01-27 19:06:41 -0700

    BUG: confused menu with class, class was set to NULL

--- main.rs
+++ main.rs
@@ -16,14 +16,17 @@ unsafe extern "system" fn winproc(wnd:HWND, a:UINT, b:WPARAM, c:LPARAM) -> LRESU
     0
 }
 
-extern "C" {
-    pub fn adder(a:c_int,b:c_int) -> c_int;
-}
+// extern "C" {
+//     pub fn adder(a:c_int,b:c_int) -> c_int;
+// }
+
+
 
 fn WinMain(hInstance : HINSTANCE,
            lpCmdLine : LPTSTR,
            nCmdShow : WORD) {
     let class = CString::new("WindowClass").unwrap();
+
     let wndclassex = WNDCLASSEX {
         cbSize:size_of::<WNDCLASSEX>() as u32,
         style: (CS_HREDRAW | CS_VREDRAW) as u32,
@@ -34,13 +37,40 @@ fn WinMain(hInstance : HINSTANCE,
         hIcon: LoadIcon(hInstance, IDI_APPLICATION),
         hCursor: LoadIcon(hInstance, IDC_ARROW),
         hbrBackground: GetStockObject(BLACK_BRUSH),
-        lpszMenuName: class.as_ptr() as LPCVOID,
-        lpszClassName: NULL,
+        lpszMenuName: NULL,//class.as_ptr() as LPCVOID,
+        lpszClassName: class.as_ptr() as LPCVOID,
         hIconSm: LoadIcon(hInstance, IDI_APPLICATION)
     };
+
     let class_atom : ATOM = RegisterClassEx(&wndclassex);
+    println!("{:?}", wndclassex.style);
+    println!("{:?}", class_atom);
+    let title = CString::new("Lustrious Paint").unwrap();
+    let wndstyle = WS_POPUP | 
+                   WS_CLIPCHILDREN | 
+                   WS_CLIPSIBLINGS | 
+                   WS_SYSMENU | 
+                   WS_THICKFRAME | 
+                   WS_GROUP | /*WS_TABSTOP |*/ 
+                   WS_BORDER | 
+                   WS_MINIMIZEBOX | 
+                   WS_MAXIMIZEBOX;
+
+    CreateWindowEx(WS_EX_APPWINDOW,
+                   class.as_ptr() as LPCTSTR,
+                   title.as_ptr() as LPCTSTR,
+                   wndstyle,
+                   100,
+                   100,
+                   640,
+                   480,
+                   NULL,
+                   NULL,
+                   hInstance,
+                   NULL);
 
     let mut msg: MSG = MSG::default();
+    println!("{:?}", class_atom);
     //GetMessage(&mut msg, NULL, 0, 0);
 }
 
@@ -49,7 +79,8 @@ fn main() {
     let lpCmdLine = GetCommandLine();
     let nCmdShow = GetStartupInfo().wShowWindow;
     WinMain(hInstance, lpCmdLine, nCmdShow);
-    println!("{:?}", unsafe{adder(2,2)});
+    //println!("{:?}", hInstance);
+    //println!("{:?}", unsafe{adder(2,2)});
 }
 
  
