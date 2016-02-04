f2aff3d265a7a085e0623381f3d29b0f33ffc1f4
Vlad Shcherbakov <vlad.shcherbakov@gmail.com>
2016-02-04 14:04:26 -0700

    libcbpad moved to its own branch

--- libcbpad/check.mk
+++
@@ -1,8 +0,0 @@
-
-define check-var-defined
-  $(if $($(1)),,$(error "Variable $(1) undefined!"))
-endef
-
-$(call check-var-defined,CFG_CPUTYPE)
-$(call check-var-defined,CFG_OSTYPE)
-$(call check-var-defined,CFG_BUILD_DIR)
--- libcbpad/config.mk
+++
@@ -1,4 +0,0 @@
-
-CFG_CPUTYPE   ?= x86-64
-CFG_OSTYPE    ?= linux
-CFG_BUILD_DIR := ./build
