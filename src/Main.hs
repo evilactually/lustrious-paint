fb4e030edeb9768c4b44527b3322fa3a7cd06185
Vlad Shcherbakov <vlad.shcherbakov@gmail.com>
2016-03-19 21:55:18 -0600

    Switch to stack, WNDCLASSEX ported

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,47 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
 
-module Main where 
+module Main where
 
-import Windows
-import Data.ByteString
-import Data.String
-import Foreign.Ptr
-
---p = intPtrToPtr (fromInteger 42 :: IntPtr)
---main = print p
-
-main = print =<< (c_GetModuleHandleA nullPtr)
-
+--main :: IO ()
 --main = do
--- p <- (c_GetModuleHandleA nullPtr)
--- print p
+--  putStrLn "hello world..."
+
+import Data.Char(toUpper)
+import Foreign.C.Types
+import Foreign.Ptr
+import Data.Void
+import Foreign.C.String
+
+-- WNDCLASSEX
+-- main = interact (map toUpper . (++) "Your data, in uppercase, is:\n\n")
+
+data AAA = FunPtr Int
+
+type HANDLE = Ptr Void
+
+type HINSTANCE = HANDLE
+
+type HICON = HANDLE
+
+type HCURSOR = HICON
+
+type HBRUSH = HANDLE
+
+type LPCTSTR = CString
+
+data WNDCLASSEX = WNDCLASSEX { cbSize :: CUInt,
+                               style :: CUInt,
+                               wndProc :: FunPtr (Ptr Void), 
+                               cbClsExtra :: CInt,
+                               cbWndExtra :: CInt,
+                               hInstance :: HINSTANCE,
+                               hIcon :: HICON,
+                               hCursor :: HCURSOR,
+                               hbrBackground :: HBRUSH,
+                               lpszMenuName :: LPCTSTR,
+                               lpszClassName:: LPCTSTR,
+                               hIconSm :: HICON }
+
+type PWNDCLASSEX = Ptr WNDCLASSEX
+
+main = interact (unlines . filter (elem 'a') . lines)
