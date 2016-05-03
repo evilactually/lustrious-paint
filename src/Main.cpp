3f04a34dc51c50c516b6a8b5d677898c204ee4cb
Vlad Shcherbakov <vlad.shcherbakov@gmail.com>
2016-05-03 00:37:48 -0600

    More initialization; version macro; switch to use C++ vulkan wrapper; Tools.h

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,51 +1,76 @@
 #include <windows.h>
 #include <vulkan/vulkan.h>
 #include <memory>
+#include <array>
 #include <vector>
+#include <assert.h>
 #include "Version.h"
+#include "Tools.h"
+#include "vulkan/vk_cpp.hpp"
+
+#include <iostream>
 
 #define ENABLE_VALIDATION true
 
-VkInstance       gInstance;
-VkPhysicalDevice gPhysicalDevice;
-VkDevice         gDevice;
-VkQueue          gGraphicsQueue;
+vk::Instance       instance;
+vk::PhysicalDevice physicalDevice;
+vk::Device         device;
+vk::Queue          graphicsQueue;
+vk::PhysicalDeviceProperties physicalDeviceProperties;
+vk::Format         depthFormat;
 
 void InitializeVulkan() {
 	// Create instance
-	VkApplicationInfo appInfo = {};
-	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.pApplicationName = PRODUCT_NAME;
-	appInfo.pEngineName = PRODUCT_NAME;
-	appInfo.apiVersion = VK_API_VERSION_1_0;
-	
+	uint32_t version = (VERSION_REVISION << 16) | (VERSION_MINOR << 8) | VERSION_MAJOR;
+	auto appInfo = vk::ApplicationInfo(PRODUCT_NAME, VERSION, PRODUCT_NAME, version, VK_API_VERSION_1_0);
+
 	std::vector<const char*> enabledExtensions = { VK_KHR_SURFACE_EXTENSION_NAME,
 		                                           VK_KHR_WIN32_SURFACE_EXTENSION_NAME };
-
-	std::vector<const char*> validationLayerNames = { "VK_LAYER_LUNARG_standard_validation" };
-
-	VkInstanceCreateInfo instanceCreateInfo = {};
-	instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	instanceCreateInfo.pNext = NULL;
-	instanceCreateInfo.pApplicationInfo = &appInfo;
-	
-	if (enabledExtensions.size() > 0)
-	{
-		if (ENABLE_VALIDATION)
-		{
-			enabledExtensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
-		}
-		instanceCreateInfo.enabledExtensionCount = enabledExtensions.size();
-		instanceCreateInfo.ppEnabledExtensionNames = enabledExtensions.data();
-	}
+	std::vector<const char*> validationLayerNames;
 
 	if (ENABLE_VALIDATION)
 	{
-		instanceCreateInfo.enabledLayerCount = validationLayerNames.size();
-		instanceCreateInfo.ppEnabledLayerNames = validationLayerNames.data();
+		validationLayerNames.push_back("VK_LAYER_LUNARG_standard_validation");
+		enabledExtensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
 	}
 
-	vkCreateInstance(&instanceCreateInfo, nullptr, &gInstance);
+	auto instanceCreateInfo = vk::InstanceCreateInfo(vk::InstanceCreateFlags(), 
+		                                             &appInfo, 
+		                                             validationLayerNames.size(), 
+		                                             validationLayerNames.data(),
+		                                             enabledExtensions.size(),
+		                                             enabledExtensions.data());
+	instance = vk::createInstance(instanceCreateInfo);
+
+	// Physical device
+	physicalDevice = instance.enumeratePhysicalDevices()[0];
+	
+	// Find a queue that supports graphics operations
+	uint32_t graphicsQueueIndex = 0;
+	auto queueProperties = physicalDevice.getQueueFamilyProperties();
+	assert(queueProperties.size() > 0);
+	for (graphicsQueueIndex = 0; graphicsQueueIndex < queueProperties.size(); graphicsQueueIndex++)
+	{
+		if (queueProperties[graphicsQueueIndex].queueFlags() & vk::QueueFlagBits::eGraphics) {
+			break;
+		}
+	}
+	assert(graphicsQueueIndex < queueProperties.size());
+
+	// Create logical device and request a single queue from graphics family
+	std::array<float, 1> queuePriorities = { 0.0f };
+	auto queueCreateInfo = vk::DeviceQueueCreateInfo(vk::DeviceQueueCreateFlags(), graphicsQueueIndex, 1, queuePriorities.data());
+
+	auto deviceCreateInfo = vk::DeviceCreateInfo(vk::DeviceCreateFlags(), 
+		                                         1, &queueCreateInfo, 
+		                                         validationLayerNames.size(), validationLayerNames.data(), 
+		                                         enabledExtensions.size(), enabledExtensions.data(), 
+		                                         nullptr);
+	device = physicalDevice.createDevice(deviceCreateInfo, nullptr);
+	physicalDeviceProperties = physicalDevice.getProperties();
+	graphicsQueue = device.getQueue(graphicsQueueIndex, 0);
+	VkBool32 validDepthFormat = Tools::getSupportedDepthFormat(physicalDevice, &depthFormat);
+	assert(validDepthFormat);
 }
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
---
+++ src/Tools.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "vulkan/vk_cpp.hpp"
+#include <windows.h>
+#include <string>
+
+namespace Tools {
+	void ExitFatal(std::string message, std::string caption) 
+	{
+		MessageBox(NULL, message.c_str(), caption.c_str(), MB_OK | MB_ICONERROR);
+		exit(1);
+	}
+
+	VkBool32 getSupportedDepthFormat(vk::PhysicalDevice physicalDevice, vk::Format* depthFormat)
+	{
+		// Since all depth formats may be optional, we need to find a suitable depth format to use
+		// Start with the highest precision packed format
+		std::vector<vk::Format> depthFormats = { 
+			vk::Format::eD32SfloatS8Uint,
+			vk::Format::eD32Sfloat,
+			vk::Format::eD24UnormS8Uint,
+            vk::Format::eD16UnormS8Uint, 
+			vk::Format::eD16Unorm
+		};
+
+		for (auto& format : depthFormats)
+		{
+			vk::FormatProperties formatProps;
+			physicalDevice.getFormatProperties(format, &formatProps);
+			// Format must support depth stencil attachment for optimal tiling
+			if (formatProps.optimalTilingFeatures() & vk::FormatFeatureFlagBits::eDepthStencilAttachment)
+			{
+				*depthFormat = format;
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
