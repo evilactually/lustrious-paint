6046c088d46fd3e3d3903205ff8f81a729244491
Vlad Shcherbakov <vlad.shcherbakov@gmail.com>
2016-12-06 17:26:26 -0700

    Pattern matcher implemented, based on iterating 24 different permutations. (Untested)

--- Libraries/LsStuffing/Source/LsIsosurfaceStuffer.cpp
+++ Libraries/LsStuffing/Source/LsIsosurfaceStuffer.cpp
@@ -465,23 +465,120 @@ void aaa() {
   // .red().edge(1,2).cut()
 }
 
-class PatternMatcher
+using LsBCCMaybeColor = LsOptional<LsBCCColor>;
+
+class PatternMatcher // NOTE: we don't need to mark cut, + - edges are always cut, everything else is not
 {
-public:
-  PatternMatcher();
-  ~PatternMatcher();
-  //PatternDSL& RecordPattern();
+private:
+  LsBCCNode matchedNodes[4];
+  std::tuple<int, LsBCCValue> patternNodes[4];
+  int currentNodeIndex = 0;
+  std::tuple<int, int, LsBCCMaybeColor> patternEdges[6];
+  int currentEdgeIndex = 0;
+  LsBCCLattice const* pLattice;
+protected:
   void AddNode(int id, LsBCCValue value);
-  void AddEdge(int id1, int id2, LsBCCValue value, bool isCut);
-  void ClearPattern();
-  void Match(LsBCCLattice const& lattice, LsBCCTetrahedron tetrahedron);
+  void AddEdge(int id1, int id2, LsBCCMaybeColor maybeColor);
+public:
+  PatternMatcher() {};
+  ~PatternMatcher() {};
+  bool Match(LsBCCLattice const& lattice, LsBCCTetrahedron tetrahedron);
   LsBCCNode GetNodeById(int id);
-  glm::vec3 GetNodePosition(int id);             // Convinience functions
+  glm::vec3 GetNodePosition(int id);             // Convenience functions
   glm::vec3 GetEdgeCutPoint(int id1, int id2);
   LsBCCColor GetNodeColor(int id);
   LsBCCColor GetEdgeColor(int id1, int id2);
   LsBCCValue GetNodeValue(int id);
 };
+
+void PatternMatcher::AddNode(int id, LsBCCValue value) {
+  assert(currentNodeIndex < 4);
+  patternNodes[currentNodeIndex++] = {id, value};
+}
+
+void PatternMatcher::AddEdge(int id1, int id2, LsBCCMaybeColor maybeColor) {
+  assert(currentEdgeIndex < 6);
+  patternEdges[currentEdgeIndex++] = {id1, id2, maybeColor};
+}
+
+LsBCCNode PatternMatcher::GetNodeById(int id) {
+  for (int i = 0; i < 4; ++i)
+  {
+    if (std::get<0>(patternNodes[i]) == id) {
+      return matchedNodes[i];
+    }
+  }
+}
+
+LsBCCColor PatternMatcher::GetEdgeColor(int id1, int id2) {
+  return pLattice->GetEdgeColor(LsBCCEdge(GetNodeById(id1), GetNodeById(id2)));
+}
+
+bool PatternMatcher::Match(LsBCCLattice const& lattice, LsBCCTetrahedron tetrahedron) {
+  pLattice = &lattice;
+  
+  // Fill permutation array with initial values
+  matchedNodes[0] = std::get<0>(tetrahedron); // Node stored at index 0 in matchedNodes
+  matchedNodes[1] = std::get<1>(tetrahedron); // corresponds to id stored at patternNodes
+  matchedNodes[2] = std::get<2>(tetrahedron); // under same index.
+  matchedNodes[3] = std::get<3>(tetrahedron);
+
+  for (int i = 0; i < 24; ++i) // This loop will try to permute matchedNodes until the pattern fits it.
+  {
+    NthPermutation(&matchedNodes[0], &matchedNodes[4], i); // Integer tuples implement lexicographical comparison.
+
+    bool failed = false;
+    
+    // Test that nodes have correct values
+    for (int j = 0; j < 4; ++j)
+    {
+      if (lattice.GetNodeValue(matchedNodes[j]) != std::get<1>(patternNodes[j])) {
+        failed = true;
+        break;
+      }
+    }
+
+    // Try next permutation
+    if (failed) continue;
+
+    // Test edge colors
+    for (int j = 0; j < 6; ++j)
+    {
+      LsBCCMaybeColor patternEdgeColor = std::get<2>(patternEdges[j]);
+
+      if (!patternEdgeColor) continue; // Pattern accepts any color for this edge, no test.
+
+      int id1 = std::get<0>(patternEdges[j]);
+      int id2 = std::get<1>(patternEdges[j]);
+      if ( GetEdgeColor(id1, id2) != patternEdgeColor ) {
+        failed = true;
+        break;
+      }
+    }
+
+    if(!failed) return true; // Found.
+  }
+  return false; // Not found.
+}
+
+class PPPPMatcher: public PatternMatcher {
+public:
+  PPPPMatcher();
+  ~PPPPMatcher() {};
+};
+
+PPPPMatcher::PPPPMatcher() {
+  AddNode(1, LsBCCValue::ePositive);
+  AddNode(2, LsBCCValue::ePositive);
+  AddNode(3, LsBCCValue::ePositive);
+  AddNode(4, LsBCCValue::ePositive);
+  AddEdge(1, 2, LsBCCMaybeColor::None());
+  AddEdge(2, 3, LsBCCMaybeColor::None());
+  AddEdge(3, 1, LsBCCMaybeColor::None());
+  AddEdge(4, 1, LsBCCMaybeColor::None());
+  AddEdge(4, 2, LsBCCMaybeColor::None());
+  AddEdge(4, 3, LsBCCMaybeColor::None());
+}
 
 /*
   Node('+', 1) 
